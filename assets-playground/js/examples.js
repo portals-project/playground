036d26d7bf9b81e1e76035fbdafe9ebdcb2c2ed6
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-07-03 18:10:27 +0200

    New playground examples (#17)
    * Update playground runtime
    * New examples on playground
    * Commented out the under construction alter

--- assets-playground/js/examples.js
+++ assets-playground/js/examples.js
@@ -92,6 +92,201 @@ var __billionEvents__ =
     'system.launch(billionEvents)\n' +
     'system.stepUntilComplete()\n'
 
+var __multiDataflowExample__ =
+    'let builder = PortalsJS.ApplicationBuilder("multiDataflow")\n' +
+    `\n` +
+    'let sequencer = builder.sequencers.random()\n' +
+    `\n` +
+    'let workflow1 = builder.workflows\n' +
+    '  .source(sequencer.stream)\n' +
+    '  .flatMap(ctx => x => { if (x <= 0) { return []; } else { return [x - 1]; } })\n' +
+    '  .logger("workflow1: ")\n' +
+    `  .sink()\n` +
+    '  .freeze()\n' +
+    'let generator = builder.generators.signal(8)\n' +
+    'builder.connections.connect(generator.stream, sequencer)\n' +
+    'builder.connections.connect(workflow1.stream, sequencer)\n' +
+    `\n` +
+    'let workflow2 = builder.workflows\n' +
+    '  .source(workflow1.stream)\n' +
+    '  .map(ctx => x => { return x * x; })\n' +
+    '  .logger("workflow2: ")\n' +
+    `  .sink()\n` +
+    '  .freeze()\n' +
+    `\n` +
+    'let workflow3 = builder.workflows\n' +
+    '  .source(workflow1.stream)\n' +
+    '  .map(ctx => x => { return x * x * x; })\n' +
+    '  .logger("workflow3: ")\n' +
+    `  .sink()\n` +
+    '  .freeze()\n' +
+    `\n` +
+    'let multiDataflow = builder.build()\n' +
+    'let system = PortalsJS.System()\n' +
+    'system.launch(multiDataflow)\n' +
+    'system.stepUntilComplete()\n'
+
+var __pingPongExample__ =
+    'var builder = PortalsJS.ApplicationBuilder("pingPong")\n' +
+    'var portal = builder.portal.portal("pingPongPortal")\n' +
+    'var generator = builder.generators.signal(8)\n' +
+    `\n` +
+    'function pingerPonger(ctx, x) {\n' +
+    '  if (x > 0) {\n' +
+    '    ctx.log.info(x);\n' +
+    '    var future = ctx.ask(portal, x - 1);\n' +
+    '    ctx.await(future, ctx => {\n' +
+    '      var v = future.value(ctx);\n' +
+    '      ctx.reply(pingerPonger(ctx, v));\n' +
+    '    });\n' +
+    '  }\n' +
+    '  else {\n' +
+    '    ctx.reply(x);\n' +
+    '  }\n' +
+    '}\n' +
+    `\n` +
+    'var pingPongWorkflow = builder.workflows\n' +
+    '  .source(generator.stream)\n' +
+    '  .askerreplier(\n' +
+    '    portal,\n' +
+    '    portal,\n' +
+    '    ctx => x => {\n' +
+    '      var future = ctx.ask(portal, x);\n' +
+    '      ctx.await(future, ctx => {\n' +
+    '        ctx.log.info("Done!");\n' +
+    '      });\n' +
+    '    },\n' +
+    '    ctx => x => {\n' +
+    '      pingerPonger(ctx, x)\n' +
+    '    },\n' +
+    '  )\n' +
+    '  .logger()\n' +
+    `  .sink()\n` +
+    '  .freeze()\n' +
+    `\n` +
+    'var pingPong = builder.build()\n' +
+    'var system = PortalsJS.System()\n' +
+    'system.launch(pingPong)\n' +
+    'system.stepUntilComplete()\n'
+
+var __portalAggregationExample__ =
+
+    'let builder = PortalsJS.ApplicationBuilder("portalAggregation")\n' +
+    `\n` +
+    'let portal = builder.portal.portal("portal", x => { return 0; })\n' +
+    `\n` +
+    'let generator = builder.generators.fromRange(0, 128, 8)\n' +
+    `\n` +
+    'let aggregationWorkflow = builder.workflows\n' +
+    '  .source(generator.stream)\n' +
+    '  .key(x => { return 0; })\n' +
+    '  .replier(\n' +
+    '    portal,\n' +
+    '    ctx => x => {\n' +
+    '      // aggregate state from generator input\n' +
+    '      let state = PortalsJS.PerKeyState("state", 0, ctx);\n' +
+    '      state.set(x + state.get());\n' +
+    '      ctx.emit(state.get());\n' +
+    '    },\n' +
+    '    ctx => x => {\n' +
+    '      // reply to portal requests with the aggregated state\n' +
+    '      let state = PortalsJS.PerKeyState("state", 0, ctx);\n' +
+    '      ctx.reply(state.get());\n' +
+    '    },\n' +
+    '  )\n' +
+    '  .sink()\n' +
+    '  .freeze()\n' +
+    `\n` +
+    `let trigger = builder.generators.fromArrayOfArrays([[0], [0], [0], [0], [0], [0], [0], [0], [0]])\n` +
+    `\n` +
+    'let requestingWorkflow = builder.workflows\n' +
+    '  .source(trigger.stream)\n' +
+    '  .asker(\n' +
+    '    portal,\n' +
+    '    ctx => x => {\n' +
+    '      // query the portal for the aggregated state\n' +
+    '      let future = ctx.ask(portal, x);\n' +
+    '      ctx.await(future, ctx => {\n' +
+    '        ctx.emit(future.value(ctx));\n' +
+    '      });\n' +
+    '    },\n' +
+    '  )\n' +
+    '  .logger("requestingWorkflow: ")\n' +
+    `  .sink()\n` +
+    '  .freeze()\n' +
+    `\n` +
+    'let portalAggregation = builder.build()\n' +
+    'let system = PortalsJS.System()\n' +
+    'system.launch(portalAggregation)\n' +
+    'system.stepUntilComplete()\n'
+
+var __portalServiceExample__ =
+    'let builder = PortalsJS.ApplicationBuilder("portalService")\n' +
+    `\n` +
+    'let portal = builder.portal.portal("portal", x => { return 0; })\n' +
+    'let generator = builder.generators.empty()\n' +
+    'let replierWorkflow = builder.workflows\n' +
+    '  .source(generator.stream)\n' +
+    '  .replier(\n' +
+    '    portal,\n' +
+    '    ctx => x => { },\n' +
+    '    ctx => x => {\n' +
+    '      let state = PortalsJS.PerKeyState("prev", 0, ctx);\n' +
+    '      let prev = state.get();\n' +
+    '      ctx.reply(prev);\n' +
+    '      state.set(x);\n' +
+    '    },\n' +
+    '  )\n' +
+    '  .sink()\n' +
+    '  .freeze()\n' +
+    `\n` +
+    'let trigger = builder.generators.fromRange(0, 128, 8)\n' +
+    'let requesterWorkflow = builder.workflows\n' +
+    '  .source(trigger.stream)\n' +
+    '  .asker(\n' +
+    '    portal,\n' +
+    '    ctx => x => {\n' +
+    '      let future = ctx.ask(portal, x);\n' +
+    '      ctx.await(future, ctx => {\n' +
+    '        ctx.emit(future.value(ctx));\n' +
+    '      });\n' +
+    '    },\n' +
+    '  )\n' +
+    '  .logger("requesterWorkflow: ")\n' +
+    `  .sink()\n` +
+    '  .freeze()\n' +
+    `\n` +
+    'let portalService = builder.build()\n' +
+    'let system = PortalsJS.System()\n' +
+    'system.launch(portalService)\n' +
+    'system.stepUntilComplete()\n'
+
+
+var __registryExample__ =
+    'let builder1 = PortalsJS.ApplicationBuilder("app1")\n' +
+    'let generator1 = builder1.generators.fromRange(0, 16, 1)\n' +
+    'let workflow1 = builder1.workflowsWithName("workflow1")\n' +
+    '  .source(generator1.stream)\n' +
+    '  .map(ctx => x => x * x)\n' +
+    '  .sink()\n' +
+    '  .freeze()\n' +
+    'let app1 = builder1.build()\n' +
+    `\n` +
+    'let builder2 = PortalsJS.ApplicationBuilder("app2")\n' +
+    'let app1Stream = builder2.registry.streams.get("/app1/workflows/workflow1/stream")\n' +
+    'let workflow2 = builder2.workflows\n' +
+    '  .source(app1Stream)\n' +
+    '  .map(ctx => x => x * x)\n' +
+    '  .logger()\n' +
+    '  .sink()\n' +
+    '  .freeze()\n' +
+    'let app2 = builder2.build()\n' +
+    `\n` +
+    'let system = PortalsJS.System()\n' +
+    'system.launch(app1)\n' +
+    'system.launch(app2)\n' +
+    'system.stepUntilComplete()\n'
+
 // SWAPPING EXAMPLE PROGRAMS:
 
 function helloVLDBExample() {
@@ -116,4 +311,24 @@ function sleepingBeautyExample() {
 
 function billionEventsExample() {
     jsEditor.setValue(__billionEvents__);
+}
+
+function multiDataflowExample() {
+    jsEditor.setValue(__multiDataflowExample__);
+}
+
+function pingPongExample() {
+    jsEditor.setValue(__pingPongExample__);
+}
+
+function portalAggregationExample() {
+    jsEditor.setValue(__portalAggregationExample__);
+}
+
+function portalServiceExample() {
+    jsEditor.setValue(__portalServiceExample__);
+}
+
+function registryExample() {
+    jsEditor.setValue(__registryExample__);
 }
